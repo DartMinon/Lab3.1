ae8c13bfa51ccc95dc9e1e84822ef453a0ece4de
Илья Абрамёнок <i.abramenok@slata.com>
2022-06-07 17:35:08 +0800

    Добавьте файлы проекта.

---
+++ Lab3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Lab3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int k1 = 11;
+            int k2 = 120;
+            Console.WriteLine("{0}", Program.gcd(k1, k2));
+        }
+
+        public static int gcd(int k1, int k2)
+            {
+           
+            while (k1 != k2)
+            {
+                if (k1 > k2) k1 -= k2;
+                else k2 -= k1;
+            }
+            int k3 = k1;
+            return k3;
+        }
+
+
+    }
+}
